6d7d41b37f8680814d06d6a3432f19becda346ab
Yosef Malul <yosefmalul@MacBook-Air-sl-yosef.local>
2026-01-12 04:46:30 +0200

    Hard reset for filename casing

--- backend/dist/routes/agent.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const agent_controller_1 = require("../controllers/agent.controller");
-const router = (0, express_1.Router)();
-const agentController = new agent_controller_1.AgentController();
-router.post('/', agentController.handleAgentMessage);
-exports.default = router;
-//# sourceMappingURL=agent.routes.js.map
